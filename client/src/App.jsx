91cb065ef4618c21412ef6c9946b62267afe5abd
Ayushjaiswal03 <ayush60000@gmail.com>
2025-08-18 19:34:28 +0530

    added protected route and created NotFound page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
-import React, { lazy } from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import React, { lazy, Suspense } from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import ProtectRoute from './components/auth/ProtectRoute.jsx';
 // import Home from './pages/home.jsx'
 // import Login from './pages/Login.jsx'
 // import Groups from './pages/Groups.jsx'
@@ -10,19 +11,28 @@ const Home = lazy( () => import('./pages/home.jsx') );
 const Login = lazy(() => import('./pages/Login.jsx')); 
 const Groups = lazy(() => import('./pages/Groups.jsx'));
 const Chat = lazy(() => import('./pages/Chat.jsx'));
+const NotFound = lazy(() => import('./pages/NotFound.jsx'));
+
+let user = true;
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/chat/:chatId" element={<Chat />} />
-        <Route path="/groups" element={<Groups />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<h1>404 Not Found</h1>} />
+    <Suspense fallback={<h1>Loading...</h1>}>
+    <Routes>
+        <Route element={<ProtectRoute user={user} />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/chat/:chatId" element={<Chat />} />
+          <Route path="/groups" element={<Groups />} />
+        </Route>
+        <Route path="/login" element={<ProtectRoute user={!user} redirect='/'>
+          <Login />
+        </ProtectRoute>} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
+    </Suspense>
     </BrowserRouter>
-    )
-}
+    );
+};
 
 export default App 
